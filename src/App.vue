caa042cf860a3b0a2e4a38550a03961d001f0bc9
Дарья Иванова <dashann@mail.ru>
2022-11-15 22:47:55 +0300

    feat/init project

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-app>
+    <v-main>
+      <ContentBlock/>
+    </v-main>
+    <v-footer
+      app
+      :absolute="true"
+      :fixed="false"
+      :padless="true"
+    >
+      <Footer />
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+import ContentBlock from "@/components/ContentBlock";
+import Footer from "@/components/Footer";
+
+export default {
+  name: 'App',
+
+  components: {
+    ContentBlock,
+    Footer
+  },
+
+  data: () => ({
+    //
+  }),
+
+  computed: {
+    localAttrs () {
+      const attrs = {}
+      attrs.absolute = true
+      attrs.fixed = false
+      return attrs
+    },
+  },
+}
+</script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-card
+    flat
+    width="100%"
+    color="red"
+    class="lighten-1 text-center"
+  >
+    <v-divider></v-divider>
+
+    <v-card-text class="white--text">
+      {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+  data: () => ({}),
+}
+</script>
